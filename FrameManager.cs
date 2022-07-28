885aedf7f3d026e55ac0eae36da7b990f55c7d98
Connor Harrison <connorh315@gmail.com>
2022-07-28 11:26:57 +0100

    Add project files.

---
+++ FrameManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+
+namespace Hologram
+{
+    public class FrameManager
+    {
+        public FrameManager()
+        {
+            sw.Start();
+            FrameTime = (float)1000 / FPS;
+        }
+
+        private static int FPS = 120;
+        private static float FrameTime;
+
+        private Stopwatch sw = new Stopwatch();
+
+        private double startFrame;
+        private double endFrame;
+
+        private double lastFrame;
+        private double drawTime;
+
+        private const int deltaSize = 500;
+        private double[] deltas = new double[deltaSize];
+        private int offset = 0;
+        private bool cycledDeltas = false;
+
+        public int avgFPS { get; private set; }
+
+        public void StartingDraw()
+        {
+            startFrame = sw.Elapsed.TotalMilliseconds;
+        }
+
+        public void FinishedDraw()
+        {
+            endFrame = sw.Elapsed.TotalMilliseconds;
+
+            drawTime = endFrame - startFrame;
+
+            double _deltaTime = endFrame - lastFrame;
+            deltas[offset] = _deltaTime;
+
+            offset++;
+            if (offset == deltaSize)
+            {
+                cycledDeltas = true;
+                offset = 0;
+            }
+
+            double totalTime = 0;
+            int sampleTo = cycledDeltas ? deltaSize : offset;
+            for (int i = 0; i < sampleTo; i++)
+            {
+                totalTime += deltas[i];
+            }
+
+            avgFPS = (int)Math.Round(sampleTo / (totalTime / 1000));
+
+            lastFrame = sw.Elapsed.TotalMilliseconds;
+        }
+
+        public void WaitUntilNextFrame()
+        {
+            double sleepyTime = FrameTime - drawTime;
+            double nextFrame = lastFrame + sleepyTime;
+            if (sleepyTime > 0)
+            {
+                Thread.Sleep((int)sleepyTime - 1);
+            }
+            while (sw.Elapsed.TotalMilliseconds < nextFrame) { }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,194 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+using System.Text;
+using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.SPIRV;
+using Veldrid.StartupUtilities;
+
+namespace Hologram
+{
+    internal class Program
+    {
+        private static GraphicsDevice _graphicsDevice;
+
+        private static CommandList _commandList;
+        private static DeviceBuffer _vertexBuffer;
+        private static DeviceBuffer _indexBuffer;
+        private static Shader[] _shaders;
+        private static Pipeline _pipeline;
+        private static FrameManager _frameManager;
+
+        static void Main(string[] args)
+        {
+            WindowCreateInfo windowCI = new WindowCreateInfo()
+            {
+                X = 100,
+                Y = 100,
+                WindowWidth = 1280,
+                WindowHeight = 720,
+                WindowTitle = "Hologram"
+            };
+            Sdl2Window window = VeldridStartup.CreateWindow(ref windowCI);
+
+            GraphicsDeviceOptions options = new GraphicsDeviceOptions()
+            {
+                PreferStandardClipSpaceYDirection = true,
+                PreferDepthRangeZeroToOne = true
+            };
+
+            _graphicsDevice = VeldridStartup.CreateGraphicsDevice(window, options);
+            CreateResources();
+
+            _frameManager = new FrameManager();
+
+            while (window.Exists)
+            {
+                _frameManager.StartingDraw();
+                window.PumpEvents();
+                Draw();
+                _frameManager.FinishedDraw();
+                
+                window.Title = "Hologram - " + _frameManager.avgFPS + " FPS";
+
+                _frameManager.WaitUntilNextFrame();
+            }
+        }
+
+        static void Draw()
+        {
+            _commandList.Begin();
+
+            _commandList.SetFramebuffer(_graphicsDevice.SwapchainFramebuffer);
+
+            _commandList.ClearColorTarget(0, RgbaFloat.Black);
+
+            _commandList.SetVertexBuffer(0, _vertexBuffer);
+            _commandList.SetIndexBuffer(_indexBuffer, IndexFormat.UInt16);
+            _commandList.SetPipeline(_pipeline);
+            _commandList.DrawIndexed(
+                indexCount: 4,
+                instanceCount: 1,
+                indexStart: 0,
+                vertexOffset: 0,
+                instanceStart: 0);
+
+            _commandList.End();
+            _graphicsDevice.SubmitCommands(_commandList);
+            _graphicsDevice.SwapBuffers();
+        }
+
+        static void DisposeResources()
+        {
+            _pipeline.Dispose();
+            _commandList.Dispose();
+            _vertexBuffer.Dispose();
+            _indexBuffer.Dispose();
+            _graphicsDevice.Dispose();
+        }
+
+        static void CreateResources()
+        {
+            ResourceFactory factory = _graphicsDevice.ResourceFactory;
+
+            VertexPositionColor[] quadVertices =
+            {
+                new VertexPositionColor(new Vector2(-0.75f, 0.75f), RgbaFloat.Red),
+                new VertexPositionColor(new Vector2(0.75f, 0.75f), RgbaFloat.Green),
+                new VertexPositionColor(new Vector2(-0.75f, -0.75f), RgbaFloat.Blue),
+                new VertexPositionColor(new Vector2(0.75f, -0.75f), RgbaFloat.Yellow),
+            };
+
+            ushort[] quadIndices = { 0, 1, 2, 3 };
+
+            _vertexBuffer = factory.CreateBuffer(new BufferDescription(4 * VertexPositionColor.SizeInBytes, BufferUsage.VertexBuffer));
+            _indexBuffer = factory.CreateBuffer(new BufferDescription(4 * 2, BufferUsage.IndexBuffer));
+
+            _graphicsDevice.UpdateBuffer(_vertexBuffer, 0, quadVertices);
+            _graphicsDevice.UpdateBuffer(_indexBuffer, 0, quadIndices);
+
+            VertexLayoutDescription vertexLayout = new VertexLayoutDescription(
+                new VertexElementDescription("Position", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float2),
+                new VertexElementDescription("Color", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float4));
+
+            ShaderDescription vertexShaderDesc = new ShaderDescription(
+                ShaderStages.Vertex,
+                Encoding.UTF8.GetBytes(VertexCode),
+                "main");
+
+            ShaderDescription fragmentShaderDesc = new ShaderDescription(
+                ShaderStages.Fragment,
+                Encoding.UTF8.GetBytes(FragmentCode),
+                "main");
+
+            _shaders = factory.CreateFromSpirv(vertexShaderDesc, fragmentShaderDesc);
+
+            GraphicsPipelineDescription pipelineDescription = new GraphicsPipelineDescription();
+            pipelineDescription.BlendState = BlendStateDescription.SingleOverrideBlend;
+
+            pipelineDescription.DepthStencilState = new DepthStencilStateDescription(
+            depthTestEnabled: true,
+            depthWriteEnabled: true,
+            comparisonKind: ComparisonKind.LessEqual);
+
+            pipelineDescription.RasterizerState = new RasterizerStateDescription(
+            cullMode: FaceCullMode.Back,
+            fillMode: PolygonFillMode.Solid,
+            frontFace: FrontFace.Clockwise,
+            depthClipEnabled: true,
+            scissorTestEnabled: false);
+
+            pipelineDescription.PrimitiveTopology = PrimitiveTopology.TriangleStrip;
+
+            pipelineDescription.ResourceLayouts = System.Array.Empty<ResourceLayout>();
+
+            pipelineDescription.ShaderSet = new ShaderSetDescription(
+            vertexLayouts: new VertexLayoutDescription[] { vertexLayout },
+            shaders: _shaders);
+
+            pipelineDescription.Outputs = _graphicsDevice.SwapchainFramebuffer.OutputDescription;
+
+            _pipeline = factory.CreateGraphicsPipeline(pipelineDescription);
+
+            _commandList = factory.CreateCommandList();
+        }
+
+        private const string VertexCode = @"
+        #version 450
+
+        layout(location = 0) in vec2 Position;
+        layout(location = 1) in vec4 Color;
+
+        layout(location = 0) out vec4 fsin_Color;
+
+        void main()
+        {
+            gl_Position = vec4(Position, 0, 1);
+            fsin_Color = Color;
+        }";
+
+        private const string FragmentCode = @"
+        #version 450
+
+        layout(location = 0) in vec4 fsin_Color;
+        layout(location = 0) out vec4 fsout_Color;
+
+        void main()
+        {
+            fsout_Color = fsin_Color;
+        }";
+    }
+
+
+    struct VertexPositionColor
+    {
+        public Vector2 Position; // This is the position, in normalized device coordinates.
+        public RgbaFloat Color; // This is the color of the vertex.
+        public VertexPositionColor(Vector2 position, RgbaFloat color)
+        {
+            Position = position;
+            Color = color;
+        }
+        public const uint SizeInBytes = 24;
+    }
+}
