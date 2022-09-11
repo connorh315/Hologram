d60977c2564f0123acd6168ddb8a29cb5669f6aa
Connor Harrison <connorh315@gmail.com>
2022-09-11 16:07:37 +0100

    Complete DNO parsing for all level files (needs convex meshes to be loaded still)

--- Hologram/FileTypes/DNO/DNOStatic.cs
+++ Hologram/FileTypes/DNO/DNOStatic.cs
@@ -27,7 +27,7 @@ namespace Hologram.FileTypes.DNO
 
                 for (int i = 0; i < nameCount; i++)
                 {
-                    Logger.Log(new LogSeg(file.ReadString(file.ReadInt(true)), ConsoleColor.Gray));
+                    /*Logger.Log(new LogSeg(*/file.ReadString(file.ReadInt(true))/*, ConsoleColor.Gray))*/;
                 }
 
                 ushort unknown = file.ReadUshort(true); // Not sure, (2)
@@ -44,6 +44,7 @@ namespace Hologram.FileTypes.DNO
                 uint vectorCount = file.ReadUint(true); // Not sure, (F)
 
                 uint unknownVal = file.ReadUint(true);
+
                 for (int i = 0; i < numItems; i++)
                 { // LinearDamping, AngularDamping, TransformIndex, MotionType, AddToScene, PostAnimState, mMassMode, HasController, Type, IntertiaMode, TerrainLayer, Inertia[0], Inertia[1], Inertia[2], numLockedVerts, 
                     float linearDamping = file.ReadFloat(true);
@@ -63,6 +64,7 @@ namespace Hologram.FileTypes.DNO
                     uint numLockedVerts = file.ReadUint(true);
                     for (int lockedCount = 0; lockedCount < numLockedVerts; lockedCount++)
                     {
+                        break;
                         // Looks like I need to seek back to "anotherUnknown" and read the values from there, but I think this means there can only be 2 lockedVerts???
                         //ushort lockedIndex = file.ReadUshort(true); // Not sure
                     }
@@ -85,7 +87,14 @@ namespace Hologram.FileTypes.DNO
 
                 //uint aZero = file.ReadUint(true);
                 ushort aTwo = file.ReadUshort(true);
-                uint aZero2 = file.ReadUint(true);
+                if (aTwo != 0) // Might not be 2, but not zero, probably should set up a condition and loop to find
+                {
+                    //for (int i = 0; i < aTwo; i++)
+                    //{
+                    //    ushort roar = file.ReadUshort(true);
+                    //}
+                    uint aZero2 = file.ReadUint(true);
+                }
                 ushort aEight = file.ReadUshort(true);
                 ushort thisCount = file.ReadUshort(true);
                 uint aTwo2 = file.ReadUint(true);
@@ -105,23 +114,96 @@ namespace Hologram.FileTypes.DNO
                     byte unknownXXX = file.ReadByte();
                     byte isInstance = file.ReadByte();
                     byte isSecondaryShape = file.ReadByte();
-                    Console.WriteLine(extents);
+                    //Console.WriteLine(extents);
                 }
 
                 uint aZeroA = file.ReadUint(true);
                 ushort aOneA = file.ReadUshort(true);
-                uint aZeroB = file.ReadUint(true);
+                ushort bodyCount = file.ReadUshort(true);
+                ushort aZeroB = file.ReadUshort(true);
+                for (int bodyId = 0; bodyId < bodyCount; bodyId++)
+                {
+                    byte type = file.ReadByte();
+                    byte numBodies = file.ReadByte();
+                    byte numJoints = file.ReadByte();
+                    byte breakable = file.ReadByte();
+                    ushort firstBodyIndex = file.ReadUshort(true);
+                    ushort firstJointIndex = file.ReadUshort(true);
+                    byte addToScene = file.ReadByte();
+                }
                 ushort aOneB = file.ReadUshort(true);
                 uint aZeroC = file.ReadUint(true);
                 ushort aOneC = file.ReadUshort(true);
 
-                ushort type = file.ReadUshort(true);
+                ushort convexMeshCount = file.ReadUshort(true);
 
                 uint ROAR = file.ReadUint(true);
 
-                if (type == 9) { throw new Exception("Type == 9"); }
+                for (int convexMeshId = 0; convexMeshId < convexMeshCount; convexMeshId++)
+                {
+                    Vector3 firstVector = new Vector3(file.ReadFloat(true), file.ReadFloat(true), file.ReadFloat(true));
+                    ushort firstVal1 = file.ReadUshort(true);
+                    ushort secoondVal1 = file.ReadUshort(true);
+                    Vector3 secondVector = new Vector3(file.ReadFloat(true), file.ReadFloat(true), file.ReadFloat(true));
+                    ushort firstVal2 = file.ReadUshort(true);
+                    ushort secondVal2 = file.ReadUshort(true);
+                    Vector3 thirdVector = new Vector3(file.ReadFloat(true), file.ReadFloat(true), file.ReadFloat(true));
+                    ushort firstVal3 = file.ReadUshort(true);
+                    ushort secondVal3 = file.ReadUshort(true);
 
-                ushort mainMeshCount = file.ReadUshort(true); // It's either this one that's the main count or "something2"
+                    Vector3 someVector = new Vector3(file.ReadFloat(true), file.ReadFloat(true), file.ReadFloat(true));
+                    float someValue = file.ReadFloat(true);
+
+                    ushort vertFacesCount = file.ReadUshort(true);
+                    ushort vertNeighbours = file.ReadUshort(true);
+                    ushort polyCount = file.ReadUshort(true);
+                    ushort vertsCount = file.ReadUshort(true);
+                    ushort indicesCount = file.ReadUshort(true);
+
+                    Vector3[] positions = new Vector3[vertsCount];
+
+                    StringBuilder str = new StringBuilder();
+                    for (int vertId = 0; vertId < vertsCount; vertId++)
+                    {
+                        Vector3 vert = new Vector3(file.ReadFloat(true), file.ReadFloat(true), file.ReadFloat(true));
+                        positions[vertId] = vert;
+                        str.AppendLine($"v {vert.X} {vert.Y} {vert.Z}");
+                        ushort neighboursCount = file.ReadUshort(true);
+                        ushort vertIndex = file.ReadUshort(true);
+                    }
+
+                    for (int polyId = 0; polyId < polyCount; polyId++)
+                    {
+                        Vector3 planeNormal = new Vector3(file.ReadFloat(true), file.ReadFloat(true), file.ReadFloat(true));
+                        file.Seek(4, SeekOrigin.Current);
+                    }
+
+                    for (int polyId = 0; polyId < polyCount; polyId++)
+                    {
+                        ushort pointsCount = file.ReadUshort(true);
+                        ushort indicesIndex = file.ReadUshort(true);
+                    }
+
+                    for (int indicesId = 0; indicesId < indicesCount; indicesId++)
+                    {
+                        ushort index = file.ReadUshort(true);
+                    }
+                    File.WriteAllText($@"A:\convexmesh{convexMeshId}.obj", str.ToString());
+
+                    for (int vertFaceId = 0; vertFaceId < vertFacesCount; vertFaceId++)
+                    {
+                        ushort vertFace = file.ReadUshort(true);
+                    }
+
+                    for (int vertNeighbourId = 0; vertNeighbourId < vertNeighbours; vertNeighbourId++)
+                    {
+                        ushort neighbour = file.ReadUshort(true);
+                    }
+                }
+
+                short mainMeshCount = file.ReadShort(true); // Files without meshes are -1 so this needs to be a short
+                if (mainMeshCount == -1) return dno;
+
                 dno.Meshes = new DMesh[mainMeshCount];
                 for (int meshId = 0; meshId < mainMeshCount; meshId++)
                 {
@@ -208,7 +290,7 @@ namespace Hologram.FileTypes.DNO
 
                 //File.WriteAllText(@"A:\1wizardofoza_secondary.obj", str.ToString());
 
-                Console.WriteLine("Done reading at: {0}", file.Position);
+                Console.WriteLine("Done reading at: {0} / {1} ({2}%)", file.Position, file.Length, 100*(float)file.Position/file.Length);
             }
             return dno;
         }
--- Hologram/Program.cs
+++ Hologram/Program.cs
@@ -14,54 +14,43 @@ namespace Hologram
         {
             using (MainWindow window = new MainWindow())
             {
-                //window.AddMesh(OBJ.Parse(@"A:\icosphere.obj").PhysicsMesh, true);
+                #region TestDNO
+                //int successful = 0;
+                //int failed = 0;
+                //foreach (string dnoFile in Directory.EnumerateFiles(@"A:\Dimensions\EXTRACT\LEVELS\STORY", "*.DNO", SearchOption.AllDirectories))
+                //{
+                //    Console.WriteLine(dnoFile);
 
-                //Mesh mesh = new Mesh(6, 4, FaceType.Triangles);
+                //    try
+                //    {
+                //        DNO dnoTest = DNO.Parse(dnoFile, 0x1);
 
-                //mesh.Vertices[0] = new Vector3(-10, -10, 0);
-                //mesh.Vertices[1] = new Vector3(-10, 10, 0);
-                //mesh.Vertices[2] = new Vector3(0, -10, 0);
-                //mesh.Vertices[3] = new Vector3(0, 10, 0);
-                //mesh.Vertices[4] = new Vector3(10, -10, 0);
-                //mesh.Vertices[5] = new Vector3(10, 10, 5);
-
-                //mesh.Faces[0].vert1 = 0;
-                //mesh.Faces[0].vert2 = 1;
-                //mesh.Faces[0].vert3 = 2;
-
-                //mesh.Faces[1].vert1 = 1;
-                //mesh.Faces[1].vert2 = 2;
-                //mesh.Faces[1].vert3 = 3;
-
-                //mesh.Faces[2].vert1 = 2;
-                //mesh.Faces[2].vert2 = 3;
-                //mesh.Faces[2].vert3 = 4;
-
-                //mesh.Faces[3].vert1 = 4;
-                //mesh.Faces[3].vert2 = 5;
-                //mesh.Faces[3].vert3 = 3;
-
-                //window.AddMesh(mesh, true);
-
-                //DNO dno = DNO.Parse(@"A:\Dimensions\EXTRACT\LEVELS\STORY\1WIZARDOFOZ\1WIZARDOFOZA\1WIZARDOFOZA_TER.DNO", 0x1ca);
-                //DNO dno = DNO.Parse(@"A:\Dimensions\EXTRACT\LEVELS\STORY\1WIZARDOFOZ\1WIZARDOFOZB\1WIZARDOFOZB_TER.DNO", 0x1ca);
-
-                //DNO dno = DNO.Parse(@"A:\Dimensions\EXTRACT\LEVELS\STORY\2THESIMPSONS\2THESIMPSONSA\2THESIMPSONSA_TER.DNO", 0x2ae);
-                DNO dno = DNO.Parse(@"A:\Dimensions\EXTRACT\LEVELS\STORY\2THESIMPSONS\2THESIMPSONSB\2THESIMPSONSB_TER.DNO", 0x2ae);
-                foreach (DMesh mesh in dno.Meshes)
-                {
-                    window.AddMesh(mesh.GetPhysicsMesh(), true);
-                }
-
-
-
+                //        Logger.Log(new LogSeg(dnoFile, ConsoleColor.Green));
+                //        successful++;
+                //    }
+                //    catch (Exception ex)
+                //    {
+                //        if (ex.Message == "Type == 9")
+                //        {
+                //            Logger.Log(new LogSeg(dnoFile, ConsoleColor.DarkRed));
+                //        }
+                //        else
+                //        {
+                //            Logger.Log(new LogSeg(dnoFile, ConsoleColor.DarkYellow));
+                //        }
+                //        failed++;
+                //    }
+                //}
+                //Console.WriteLine("Successfully read: {0}", successful);
+                //Console.WriteLine("Failed reading: {0}", failed);
+                #endregion
 
                 //Mesh mesh = DNO.Parse(@"A:\Dimensions\EXTRACT\LEVELS\STORY\1WIZARDOFOZ\1WIZARDOFOZB\1WIZARDOFOZB_TER.DNO", 0x1ca).PhysicsMesh;
-                //window.AddMesh(DNO.Parse(@"A:\Dimensions\EXTRACT\LEVELS\HUB\VORTON\VORTON_TERR.DNO", 0x149).PhysicsMesh, true);
+                //window.AddMesh(DNO.Parse(@"A:\Dimensions\EXTRACT\LEVELS\HUB\VORTON\VORTON_TERR.DNO", 0x149).Meshes[0].GetPhysicsMesh(), true);
                 //window.AddMesh(DNO.Parse(@"A:\Dimensions\EXTRACT\LEVELS\STORY\1WIZARDOFOZ\1WIZARDOFOZA\1WIZARDOFOZA_TER.DNO", 0x151).PhysicsMesh);
                 //Mesh mesh = DNO.Parse(@"A:\Dimensions\EXTRACT\LEVELS\STORY\1WIZARDOFOZ\1WIZARDOFOZFREEPLAY\1WIZARDOFOZFREEPLAY_TER.DNO", 0x2a).PhysicsMesh;
                 //window.AddMesh(.PhysicsMesh);
-                //window.AddMesh(GSCFile.Read(@"A:\Dimensions\EXTRACT\LEVELS\STORY\1WIZARDOFOZ\1WIZARDOFOZA\1WIZARDOFOZA_NXG.GSC"));
+                window.AddMesh(GSCFile.Read(@"A:\Dimensions\EXTRACT\LEVELS\STORY\1WIZARDOFOZ\1WIZARDOFOZA\1WIZARDOFOZA_NXG.GSC"), true);
 
                 //int completed = 0;
                 //foreach (string meshable in Directory.EnumerateFiles(@"A:\Dimensions\EXTRACT\LEVELS\STORY", "*.DNO", SearchOption.AllDirectories))
