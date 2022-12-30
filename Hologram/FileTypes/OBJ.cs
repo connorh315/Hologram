899df9f5c883b6b4fd506d396e3095181d1fe6a0
Connor Harrison <connorh315@gmail.com>
2022-12-30 23:46:46 +0000

    Moved over the awful OBJ implementation from Fragment, needs updating at some point

--- Hologram/FileTypes/OBJ.cs
+++ Hologram/FileTypes/OBJ.cs
@@ -2,12 +2,13 @@
 using OpenTK.Mathematics;
 using Hologram.Objects;
 using System.Text;
+using System.Globalization;
 
 namespace Hologram.FileTypes
 {
     public class OBJ
     {
-        public Mesh PhysicsMesh;
+        public MeshX Mesh;
 
         private static Vector3 ReadVertex(ModFile file)
         {
@@ -22,12 +23,11 @@ namespace Hologram.FileTypes
                     if (read != ' ' && read != '\n') builder.Append((char)read);
                     else
                     {
-                        vertex[pos] = float.Parse(builder.ToString());
+                        vertex[pos] = float.Parse(builder.ToString(), CultureInfo.InvariantCulture.NumberFormat);
                         pos++;
                         builder.Clear();
                     }
                 }
-
 
                 if (read == '\n') return vertex;
             }
@@ -35,50 +35,63 @@ namespace Hologram.FileTypes
             return vertex;
         }
 
-        private static Face ReadFace(ModFile file)
+        private static RawFace ReadFace(ModFile file)
         {
             StringBuilder builder = new StringBuilder();
-            Face face = new Face();
+            RawFace face = new RawFace();
             byte pos = 0;
-            bool ignore = false;
+            byte type = 0;
             while (file.Position < file.Length)
             {
                 byte read = file.ReadByte();
                 if (read != ' ' && read != '\n' && read != '/') builder.Append((char)read);
-                else if (!ignore)
+                else
                 {
-                    ushort value = (ushort)(ushort.Parse(builder.ToString()) - 1); // Takeaway 1 because OBJ files start indexing at 1
-                    if (pos == 0)
-                    {
-                        face.vert1 = value;
+                    if (!int.TryParse(builder.ToString(), out int massive))
+                    { // Will return false if UVs aren't defined etc. etc.
+                        massive = 0;
                     }
-                    else if (pos == 1)
+
+                    if (massive > ushort.MaxValue) throw new System.Exception("Model has too many vertices.");
+                    if (massive > 0)
                     {
-                        face.vert2 = value;
+                        massive -= 1; // Takeaway 1 because OBJ files start indexing at 1
                     }
-                    else if (pos == 2)
+                    short value = (short)massive;
+
+                    if (type == 0) // Vertex Position
                     {
-                        face.vert3 = value;
+                        if (pos == 0) face.vert1 = value;
+                        else if (pos == 1) face.vert2 = value;
+                        else if (pos == 2) face.vert3 = value;
+                        else if (pos == 3) face.vert4 = value;
                     }
-                    else if (pos == 3)
+                    else if (type == 1) // Vertex UV
                     {
-                        face.vert4 = value;
+                        if (pos == 0) face.vert1UV = value;
+                        else if (pos == 1) face.vert2UV = value;
+                        else if (pos == 2) face.vert3UV = value;
+                        else if (pos == 3) face.vert4UV = value;
                     }
-                    
-                    if (pos != 4)
+                    else if (type == 2) // Vertex Normal
                     {
-                        pos++;
-                        builder.Clear();
+                        if (pos == 0) face.vert1Norm = value;
+                        else if (pos == 1) face.vert2Norm = value;
+                        else if (pos == 2) face.vert3Norm = value;
+                        else if (pos == 3) face.vert4Norm = value;
                     }
+
+                    builder.Clear();
                 }
 
                 if (read == '/')
                 {
-                    ignore = true;
+                    type++;
                 }
                 else if (read == ' ')
                 {
-                    ignore = false;
+                    type = 0;
+                    pos++;
                     builder.Clear();
                 }
                 else if (read == '\n')
@@ -88,6 +101,71 @@ namespace Hologram.FileTypes
             }
 
             return face;
+        }
+
+        /// <summary>
+        /// Reads "vt ..." line (UV coords)
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        private static Vector2 ReadUV(ModFile file)
+        {
+            Vector2 result = new Vector2();
+            bool xSet = false;
+            StringBuilder builder = new StringBuilder();
+
+            while (file.Position < file.Length)
+            {
+                byte read = file.ReadByte();
+                if (read != ' ' && read != '\n')
+                {
+                    builder.Append((char)read);
+                }
+                else
+                {
+                    if (xSet)
+                    {
+                        result.Y = float.Parse(builder.ToString(), CultureInfo.InvariantCulture.NumberFormat);
+                        if (read != '\n') ReadThrough(file);
+                        return result;
+                    }
+                    else
+                    {
+                        result.X = float.Parse(builder.ToString(), CultureInfo.InvariantCulture.NumberFormat);
+                        xSet = true;
+                        builder.Clear();
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Reads "vn ..." line (Normal)
+        /// </summary>
+        /// <param name="file"></param>
+        private static Vector3 ReadNormal(ModFile file)
+        {
+            Vector3 result = new Vector3();
+            byte pos = 0;
+            StringBuilder builder = new StringBuilder();
+
+            while (file.Position < file.Length)
+            {
+                byte read = file.ReadByte();
+                if (read != ' ' && read != '\n') builder.Append((char)read);
+                else if (pos < 3)
+                {
+                    result[pos] = float.Parse(builder.ToString(), CultureInfo.InvariantCulture.NumberFormat);
+                    builder.Clear();
+                    pos++;
+                }
+
+                if (read == '\n') return result;
+            }
+
+            return result;
         }
 
         /// <summary>
@@ -105,10 +183,17 @@ namespace Hologram.FileTypes
             }
         }
 
+        static List<Vector3> rawVertices;
+        static List<RawFace> rawFaces;
+        static List<Vector2> rawUV;
+        static List<Vector3> rawNormals;
+
         public static OBJ Parse(string fileLocation)
         {
-            List<Vector3> vertices = new List<Vector3>();
-            List<Face> faces = new List<Face>();
+            rawVertices = new List<Vector3>();
+            rawFaces = new List<RawFace>();
+            rawUV = new List<Vector2>();
+            rawNormals = new List<Vector3>();
 
             OBJ obj = new OBJ();
 
@@ -122,7 +207,17 @@ namespace Hologram.FileTypes
                         byte next = file.ReadByte();
                         if (next == ' ')
                         {
-                            vertices.Add(ReadVertex(file));
+                            rawVertices.Add(ReadVertex(file));
+                        }
+                        else if (next == 't')
+                        {
+                            file.ReadByte(); // ' '
+                            rawUV.Add(ReadUV(file));
+                        }
+                        else if (next == 'n')
+                        {
+                            file.ReadByte(); // ' '
+                            rawNormals.Add(ReadNormal(file));
                         }
                         else
                         {
@@ -134,7 +229,7 @@ namespace Hologram.FileTypes
                         byte next = file.ReadByte();
                         if (next == ' ')
                         {
-                            faces.Add(ReadFace(file));
+                            rawFaces.Add(ReadFace(file));
                         }
                         else
                         {
@@ -147,12 +242,119 @@ namespace Hologram.FileTypes
                     }
                 }
 
-                obj.PhysicsMesh = new Mesh(vertices.Count, faces.Count, faces[0].vert4 != 0 ? FaceType.Quads : FaceType.Triangles);
-                obj.PhysicsMesh.Faces = faces.ToArray();
-                obj.PhysicsMesh.Vertices = vertices.ToArray();
+                // The capacities applied to these elements are solely a starting point, they will exceed this value.
+                vertices = new List<Vertex>(rawVertices.Count);
+                vertDictionary = new Dictionary<ushort, List<VertexID>>(rawVertices.Count);
+                List<ushort> indices = new List<ushort>(rawFaces.Count * 3);
+                if (rawUV.Count == 0)
+                {
+                    rawUV.Add(Vector2.Zero); // Dummy value for when UVs are not exported in OBJ
+                }
+
+                for (int i = 0; i < rawFaces.Count; i++)
+                {
+                    ushort vert1 = GetVertIndex(rawFaces[i].vert1, rawFaces[i].vert1UV, rawFaces[i].vert1Norm);
+                    ushort vert2 = GetVertIndex(rawFaces[i].vert2, rawFaces[i].vert2UV, rawFaces[i].vert2Norm);
+                    ushort vert3 = GetVertIndex(rawFaces[i].vert3, rawFaces[i].vert3UV, rawFaces[i].vert3Norm);
+                    ushort vert4 = rawFaces[i].vert4 == 0 ? (ushort)0xffff : GetVertIndex(rawFaces[i].vert4, rawFaces[i].vert4UV, rawFaces[i].vert4Norm);
+
+                    indices.Add(vert1);
+                    indices.Add(vert2);
+                    indices.Add(vert3);
+                    if (vert4 != 0xffff)
+                    {
+                        indices.Add(vert1);
+                        indices.Add(vert3);
+                        indices.Add(vert4);
+                    }
+                }
+
+                obj.Mesh = new MeshX();
+                obj.Mesh.Vertices = vertices.ToArray();
+                obj.Mesh.Indices = indices.ToArray();
+
+                //obj.Hash = HashCode.Combine(vertices.GetHashCode(), indices.GetHashCode()); // This is so much hassle.
 
                 return obj;
             }
+        }
+
+        static Dictionary<ushort, List<VertexID>> vertDictionary;
+        static List<Vertex> vertices;
+
+        private static ushort GetVertIndex(short rawVertIdx, short rawUVIdx, short rawNormIdx)
+        {
+            ushort vertIndex = GetIndice(rawVertIdx, rawVertices.Count);
+            ushort uvIndex = GetIndice(rawUVIdx, rawVertices.Count);
+            ushort normIndex = GetIndice(rawNormIdx, rawVertices.Count);
+
+            if (vertDictionary.ContainsKey(vertIndex))
+            {
+                List<VertexID> list = vertDictionary[vertIndex];
+                for (ushort i = 0; i < list.Count; i++)
+                {
+                    if (list[i].UV == uvIndex) return list[i].Index;
+                }
+
+                ushort id = (ushort)vertices.Count;
+                list.Add(new VertexID(uvIndex, id));
+                Vertex vertex = new Vertex(rawVertices[vertIndex], rawNormals[normIndex], rawUV[uvIndex], Color4.White);
+                vertices.Add(vertex);
+
+                return id;
+            }
+            else
+            {
+                List<VertexID> newList = new();
+                ushort id = (ushort)vertices.Count;
+                newList.Add(new VertexID(uvIndex, id));
+                vertDictionary.Add(vertIndex, newList);
+                Vertex vertex = new Vertex(rawVertices[vertIndex], rawNormals[normIndex], rawUV[uvIndex], Color4.White);
+                vertices.Add(vertex);
+
+                return id;
+            }
+        }
+
+        /// <summary>
+        /// Inverses the indice if it is negative.
+        /// </summary>
+        /// <param name="uncorrected">The raw indice that is read</param>
+        /// <param name="vertexCount">The total number of vertices</param>
+        /// <returns>Corrected indice</returns>
+        private static ushort GetIndice(short uncorrected, int vertexCount)
+        {
+            if (uncorrected >= 0) return (ushort)uncorrected;
+
+            return (ushort)(vertexCount + uncorrected);
+        }
+
+        private struct RawFace
+        {
+            public short vert1;
+            public short vert2;
+            public short vert3;
+            public short vert4;
+            public short vert1UV;
+            public short vert2UV;
+            public short vert3UV;
+            public short vert4UV;
+            public short vert1Norm;
+            public short vert2Norm;
+            public short vert3Norm;
+            public short vert4Norm;
+        }
+    }
+
+    internal class VertexID
+    {
+        public ushort UV;
+        public ushort Index;
+
+        public VertexID(ushort uv, ushort index)
+        {
+            UV = uv;
+            Index = index;
         }
     }
 }
